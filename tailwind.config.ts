ed7ccf171530b43fcdd6cdf0bc9d296cf8c96b00
Maria Luìsa <malubritoc@MacBook-Air-de-Maria.local>
2024-12-11 16:12:51 -0300

    feat: creating static progress page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,21 +11,43 @@ export default {
   	extend: {
   		colors: {
   			'dark-orange': '#f23e02',
-			'medium-orange': '#eb613b',
-			'light-orange': '#f98f6f',
-			'dark-turquoise': '#2c6b74',
-			'medium-turquoise': '#00988d',
-			'light-turquoise': '#c1d9cd',
-			'dark-blue': '#013750',
-			'baby-yellow': '#fef5c8',
-			'beige': '#f7eadc',
-			'default-gray': '#64748B',
-			'baby-gray': '#f1f5f9',
+  			'medium-orange': '#eb613b',
+  			'light-orange': '#f98f6f',
+  			'dark-turquoise': '#2c6b74',
+  			'medium-turquoise': '#00988d',
+  			'light-turquoise': '#c1d9cd',
+  			'dark-blue': '#013750',
+  			'baby-yellow': '#fef5c8',
+  			beige: '#f7eadc',
+  			'default-gray': '#64748B',
+  			'baby-gray': '#f1f5f9'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
