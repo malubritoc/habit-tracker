43d45d9de20538ff97f4223890cb307ad2e86d06
Tobias Santos <tobiassantosn@gmail.com>
2024-12-19 00:35:40 -0300

    graficos de progresso geral

--- src/components/progress/generalData.tsx
+++ src/components/progress/generalData.tsx
@@ -1,6 +1,37 @@
 import clsx from "clsx";
+import Chart from "../chart/chart";
 
 export function GeneralData() {
+  const barStackedData = [
+    { date: "15/05/2024", completed: 84, remaining: 16 },
+    { date: "16/05/2024", completed: 75, remaining: 25 },
+    { date: "17/05/2024", completed: 90, remaining: 10 },
+    { date: "18/05/2024", completed: 50, remaining: 50 },
+    { date: "19/05/2024", completed: 100, remaining: 0 },
+    { date: "20/05/2024", completed: 60, remaining: 40 },
+    { date: "21/05/2024", completed: 30, remaining: 70 },
+  ];
+
+  const lineData = [
+    { date: "15/05/2024", completed: 84, remaining: 16 },
+    { date: "16/05/2024", completed: 75, remaining: 25 },
+    { date: "17/05/2024", completed: 90, remaining: 10 },
+    { date: "18/05/2024", completed: 50, remaining: 50 },
+    { date: "19/05/2024", completed: 100, remaining: 0 },
+    { date: "20/05/2024", completed: 60, remaining: 40 },
+    { date: "21/05/2024", completed: 30, remaining: 70 },
+  ];
+
+  const pieData = [
+    { date: "15/05/2024", completed: 84, remaining: 16 },
+    { date: "16/05/2024", completed: 75, remaining: 25 },
+    { date: "17/05/2024", completed: 90, remaining: 10 },
+    { date: "18/05/2024", completed: 50, remaining: 50 },
+    { date: "19/05/2024", completed: 100, remaining: 0 },
+    { date: "20/05/2024", completed: 60, remaining: 40 },
+    { date: "21/05/2024", completed: 30, remaining: 70 },
+  ];
+
   return (
     <div
       className={clsx(
@@ -19,6 +50,25 @@ export function GeneralData() {
         </p>
       </div>
       <p className="text-sm text-default-gray font-medium">Resultados</p>
+
+      <div>
+        <h2 className="text-xs text-default-gray mb-4">
+          Progresso Semanal de Tarefas
+        </h2>
+        <Chart data={barStackedData} chartType="bar-stacked" />
+      </div>
+
+      <div>
+        <h2 className="text-xs text-default-gray mb-4">
+          Progresso das Tarefas ao Longo da Semana
+        </h2>
+        <Chart data={lineData} chartType="line" />
+      </div>
+
+      <div>
+        <h2 className="text-xs text-default-gray mb-4">Resumo das Tarefas</h2>
+        <Chart data={pieData} chartType="pie" />
+      </div>
     </div>
   );
 }
